3d21fc9f31b783c82a9d45cdee62e0027bc79c6c
Brian Nguyen <nguyenbrian92620@gmail.com>
2023-11-08 16:13:08 -0800

    minor frontend adj

--- react-frontend/src/TaskFrom.js
+++ react-frontend/src/TaskFrom.js
@@ -12,86 +12,86 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
 function TaskForm(props) {
-  const [task, setTask] = useState({
-    user: "",
-    title: "",
-    category: "",
-    description: "",
-    date: new Date(),
-    flagged: false,
-    completed: false,
-  });
+    const [task, setTask] = useState({
+        user: "",
+        title: "",
+        category: "",
+        description: "",
+        date: new Date(),
+        flagged: false,
+        completed: false,
+    });
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === "title") setTask({ title: value });
-    else if (name === "category") setTask({ category: value });
-    else if (name === "description") setTask({ description: value });
-  }
+    function handleChange(event) {
+        const { name, value } = event.target;
+        if (name === "title") setTask({ title: value });
+        else if (name === "category") setTask({ category: value });
+        else if (name === "description") setTask({ description: value });
+    }
 
-  function SelectCategory() {
+    function SelectCategory() {
+        return (
+            <FormControl variant="filled" fullWidth>
+                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={task.category}
+                    label="Category"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={"home"}>Home</MenuItem>
+                    <MenuItem value={"school"}>School</MenuItem>
+                    <MenuItem value={"sports"}>Sports</MenuItem>
+                </Select>
+            </FormControl>
+        );
+    }
+
+    function DatePicker() {
+        const [value, setDate] = React.useState(dayjs("2022-04-17"));
+        return (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar
+                    value={value}
+                    onChange={(newValue) => setDate(newValue)}
+                />{" "}
+            </LocalizationProvider>
+        );
+    }
+
+    function submitForm() {
+        props.handleSubmit(task);
+        setTask({ title: "", category: "", description: "" });
+    }
+
     return (
-      <FormControl variant="filled" fullWidth>
-        <InputLabel id="demo-simple-select-label">Category</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={task.category}
-          label="Category"
-          onChange={handleChange}
-        >
-          <MenuItem value={"home"}>Home</MenuItem>
-          <MenuItem value={"school"}>School</MenuItem>
-          <MenuItem value={"sports"}>Sports</MenuItem>
-        </Select>
-      </FormControl>
+        <form>
+            <TextField
+                label="Title"
+                variant="filled"
+                name="title"
+                value={task.title}
+                onChange={handleChange}
+                fullWidth
+                margin="normal"
+            />
+            <SelectCategory />
+            <TextField
+                label="Description"
+                variant="filled"
+                name="description"
+                value={task.description}
+                onChange={handleChange}
+                fullWidth
+                margin="normal"
+            />
+            <DatePicker />
+            <Button onClick={submitForm} variant="contained" color="primary">
+                Submit
+            </Button>
+        </form>
     );
-  }
-
-  function DatePicker() {
-    const [value, setDate] = React.useState(dayjs("2022-04-17"));
-    return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar
-          value={value}
-          onChange={(newValue) => setDate(newValue)}
-        />{" "}
-      </LocalizationProvider>
-    );
-  }
-
-  function submitForm() {
-    props.handleSubmit(task);
-    setTask({ title: "", category: "", description: "" });
-  }
-
-  return (
-    <form>
-      <TextField
-        label="Title"
-        variant="filled"
-        name="title"
-        value={task.title}
-        onChange={handleChange}
-        fullWidth
-        margin="normal"
-      />
-      <SelectCategory />
-      <TextField
-        label="Description"
-        variant="filled"
-        name="description"
-        value={task.description}
-        onChange={handleChange}
-        fullWidth
-        margin="normal"
-      />
-      <DatePicker />
-      <Button onClick={submitForm} variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
-  );
 }
 
 export default TaskForm;
--- react-frontend/src/TaskTable.js
+++ react-frontend/src/TaskTable.js
@@ -1,86 +1,178 @@
 import React, { useState } from "react";
+
 import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
 
 import IconButton from "@mui/material/IconButton";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
-
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 function DeleteButton() {
-  const handleDelete = () => {
-    // Implement the delete logic here
-  };
+    const handleDelete = () => {
+        // Implement the delete logic here
+    };
 
-  return (
-    <IconButton onClick={handleDelete} color="secondary">
-      <DeleteOutlineIcon />
-    </IconButton>
-  );
+    return (
+        <IconButton onClick={handleDelete} color="secondary">
+            <DeleteOutlineIcon />
+        </IconButton>
+    );
 }
 
 function FlagToggleButton() {
-  const [isFlagged, setIsFlagged] = useState(false);
+    const [isFlagged, setIsFlagged] = useState(false);
 
-  const handleToggleFlag = () => {
-    setIsFlagged(!isFlagged);
-  };
+    const handleToggleFlag = () => {
+        setIsFlagged(!isFlagged);
+    };
 
-  return (
-    <div>
-      <IconButton onClick={handleToggleFlag}>
-        {isFlagged ? <BookmarkIcon /> : <BookmarkBorderOutlinedIcon />}
-      </IconButton>
-    </div>
-  );
+    return (
+        <div>
+            <IconButton onClick={handleToggleFlag}>
+                {isFlagged ? <BookmarkIcon /> : <BookmarkBorderOutlinedIcon />}
+            </IconButton>
+        </div>
+    );
 }
 
 function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+    return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
+// function TaskTable() {
+//     return (
+//         <TableContainer component={Paper}>
+//             <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//                 <TableHead>
+//                     <TableRow>
+//                         <TableCell>Status</TableCell>{" "}
+//                         <TableCell>Task Name</TableCell>
+//                         <TableCell>Date</TableCell>
+//                         <TableCell>Category</TableCell>
+//                         <TableCell>Priority</TableCell>
+//                     </TableRow>
+//                 </TableHead>
+//                 <TableBody>
+//                     {rows.map((row) => (
+//                         <TableRow
+//                             key={row.name}
+//                             sx={{
+//                                 "&:last-child td, &:last-child th": {
+//                                     border: 0,
+//                                 },
+//                             }}
+//                         >
+//                             <TableCell>{row.calories}</TableCell>
+//                             <TableCell component="th" scope="row">
+//                                 {row.name}
+//                             </TableCell>
+//                             <TableCell align="right">{row.calories}</TableCell>
+//                             <TableCell align="right">{row.calories}</TableCell>
+//                             <TableCell align="right">
+//                                 <FlagToggleButton />
+//                             </TableCell>
+//                             <TableCell align="right">
+//                                 <DeleteButton />
+//                             </TableCell>
+//                         </TableRow>
+//                     ))}
+//                 </TableBody>
+//             </Table>
+//         </TableContainer>
+//     );
+// }
+
 function TaskTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-              <TableCell align="right">
-                <FlagToggleButton />
-              </TableCell>
-              <TableCell align="right">
-                <DeleteButton />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Task Name</TableCell>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Category</TableCell>
+                        <TableCell>Priority</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows
+                        .slice(
+                            page * rowsPerPage,
+                            page * rowsPerPage + rowsPerPage
+                        )
+                        .map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{
+                                    "&:last-child td, &:last-child th": {
+                                        border: 0,
+                                    },
+                                }}
+                            >
+                                <TableCell>{row.calories}</TableCell>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.calories}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.calories}
+                                </TableCell>
+                                <TableCell align="right">
+                                    <FlagToggleButton />
+                                </TableCell>
+                                <TableCell align="right">
+                                    <DeleteButton />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </TableContainer>
+    );
 }
 
 export default TaskTable;
