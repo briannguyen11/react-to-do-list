4f3b9e161e2fb6de50298e552e943114b48c8b3d
Emory Jendretzke <81583127+ejendret@users.noreply.github.com>
2023-12-03 11:15:06 -0800

    Working on implementing deployment

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -175,6 +175,6 @@ app.put("/tasks/:id", async (req, res) => {
     else res.status(500).end();
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
